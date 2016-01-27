de2b0df7b8c1414884a28872bd0881f0e112922f
epickrram <mark@epickrram.com>
2016-01-27 15:03:09 +0000

    print value of current clocksource when reporting nanoTime call latency

--- src/main/java/com/epickrram/blog/nanotiming/TimingJob.java
+++ src/main/java/com/epickrram/blog/nanotiming/TimingJob.java
@@ -2,6 +2,9 @@ package com.epickrram.blog.nanotiming;
 
 import org.HdrHistogram.Histogram;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
@@ -9,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 public final class TimingJob implements Runnable
 {
     private static final long HIGHEST_TRACKABLE_VALUE = TimeUnit.MILLISECONDS.toNanos(10L);
+    private static final String CURRENT_CLOCKSOURCE_FILENAME = "/sys/devices/system/clocksource/clocksource0/current_clocksource";
 
     private final Histogram histogram = new Histogram(HIGHEST_TRACKABLE_VALUE, 2);
 
@@ -28,6 +32,14 @@ public final class TimingJob implements Runnable
             {
                 if(lastReportedNanos != 0)
                 {
+                    try
+                    {
+                        System.out.printf("Current clocksource is: %s%n", Files.readAllLines(Paths.get(CURRENT_CLOCKSOURCE_FILENAME)).get(0));
+                    }
+                    catch (IOException e)
+                    {
+                        System.err.printf("Failed to read current clocksource from %s%n");
+                    }
                     System.out.printf("%s avg. time between calls to System.nanoTime() %dns%n",
                             formatter.format(LocalTime.now()), (long) histogram.getMean());
                 }
